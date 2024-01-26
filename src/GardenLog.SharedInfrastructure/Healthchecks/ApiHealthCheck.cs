f537629951bb30ed28669066ea3d8f891082b2ca
Steve Kravchenko <KRAVVYA@chrobinson.com>
2024-01-25 20:55:12 -0600

    Add ApiHelthCheck skeleton for the future

---
+++ src/GardenLog.SharedInfrastructure/Healthchecks/ApiHealthCheck.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace GardenLog.SharedInfrastructure.Healthchecks;
+
+public class ApiHealthCheck : IHealthCheck
+{
+    private readonly HttpClient _httpClient;
+
+    public ApiHealthCheck(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+    {
+        var reseponse = await _httpClient.GetAsync("");
+
+        if(reseponse.IsSuccessStatusCode)
+        {
+            return await Task.FromResult(new HealthCheckResult(
+                status: HealthStatus.Healthy,
+                description: "The Api is up and running"));
+        }
+        return await Task.FromResult(new HealthCheckResult(
+               status: HealthStatus.Unhealthy,
+               description: "The Api is down"));
+    }
+}
+{
+}
